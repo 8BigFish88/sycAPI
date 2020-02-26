bb4360b7809174c0c045971fa1469d931aeb0591
Ivana <ivana.zaccheddu88@gmail.com>
2020-02-26 17:37:52 +0100

    change url db

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dutufrppfjvlam:516671303cf21a6eeb9fbe7b987de74022fbeaa7c95b1961ba9569b0f8086fbd@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/d3ogkdt514ripf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['DATABASE_URL'] = 'postgres://dutufrppfjvlam:516671303cf21a6eeb9fbe7b987de74022fbeaa7c95b1961ba9569b0f8086fbd@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/d3ogkdt514ripf'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['PER_PAGE'] = 6
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)

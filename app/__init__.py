1825baa16c3b480a7c1b5d0bb19f948c3f65c9ae
Ivana <ivana.zaccheddu88@gmail.com>
2020-02-27 11:56:34 +0100

    change db

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['DATABASE_URL'] = 'postgres://rmjbezokjkyagx:8c9afbc899de855cb3e60c36f06f5740589bb2ddb4e4f5bbc5b16d4f3815fea5@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/df34oalf5k8a2j'
+app.config['DATABASE_URL'] = 'postgres://mhtsdvfq:yOhNRqRkv7XmSoYEMY4pgvM3AmrpTC-0@kandula.db.elephantsql.com:5432/mhtsdvfq'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['PER_PAGE'] = 6
 db = SQLAlchemy(app)
